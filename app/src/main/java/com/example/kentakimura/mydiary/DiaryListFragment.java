45eb1d789f1d77f07e4fb8d0225e3a075a109d57
kentakimura <kenta.kiimura@nislab.doshisha.ac.jp>
2018-09-18 13:44:46 +0900

    DiaryListFragment

--- app/src/main/java/com/example/kentakimura/mydiary/DiaryListFragment.java
+++ app/src/main/java/com/example/kentakimura/mydiary/DiaryListFragment.java
@@ -1,12 +1,16 @@
 package com.example.kentakimura.mydiary;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 
 /**
@@ -18,60 +22,47 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DiaryListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private Realm mRealm;
 
     public DiaryListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DiaryListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DiaryListFragment newInstance(String param1, String param2) {
+    public static DiaryListFragment newInstance() {
         DiaryListFragment fragment = new DiaryListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mRealm = Realm.getDefaultInstance();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mRealm.close();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary_list, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_diary_list, container, false);
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recycler);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(llm);
+
+        RealmResults<Diary> diaries = mRealm.where(Diary.class).findAll();
+        DiaryRealmAdapter adapter =
+                new DiaryRealmAdapter(getActivity(), diaries, true);
+
+        recyclerView.setAdapter(adapter);
+        return v;
     }
 
     @Override
@@ -91,18 +82,7 @@ public class DiaryListFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onAddDiarySelected();
     }
 }
